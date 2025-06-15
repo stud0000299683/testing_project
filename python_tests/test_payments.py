f4ca5eb97d7fd2a239fb664ee0f6c53202d66630
stud0000299683 <145689661+stud0000299683@users.noreply.github.com>
2025-06-15 22:01:36 +0300

    Update test_payments.py
    Поправил путь запуска

--- python_tests/test_payments.py
+++ python_tests/test_payments.py
@@ -24,10 +24,15 @@ def driver():
 
 @pytest.fixture(scope="module", autouse=True)
 def server_start():
-    process = subprocess.Popen(["C://Users//chamorcev//AppData//Local//Programs//Python//Python312//python.exe", "-m", "http.server", "8000"],
-                               cwd=os.path.join(os.path.dirname(__file__), "..", "dist"),
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
+     process = subprocess.Popen(
+        [python_path, "-m", "http.server", "8000"],
+        cwd=os.path.join(os.path.dirname(__file__), "..", "dist"),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    #process = subprocess.Popen(["C://Users//chamorcev//AppData//Local//Programs//Python//Python312//python.exe", "-m", "http.server", "8000"],
+    #                           cwd=os.path.join(os.path.dirname(__file__), "..", "dist"),
+    #                           stdout=subprocess.PIPE,
+    #                           stderr=subprocess.PIPE)
     for _ in range(10):
         try:
             urllib.request.urlopen("http://localhost:8000")
